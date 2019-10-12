5d9a65cf5751b85b729b2b3ae47ed7e8877024b7
Azod <Azod@AZODPC>
2019-10-12 12:18:33 +0500

    Добавлено Тествоое сообщение

---
+++ CONSOLE/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+namespace CONSOLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num;
+            
+            string otvet = null;
+            num = Int32.Parse(Interaction.InputBox("Введите число", "проверка на деления"));
+            //////////////////////////////////////////////////////////////////////////////
+            //otvet = num > 5 && num <= 10 ? "da" :"net";
+            num &= 247;
+            //otvet = Convert.ToString(num, 2);
+
+            //num %= 10;
+            otvet = Convert.ToString(num);
+            //otvet = Convert.ToString(otvet[otvet.Length - 1]);
+            
+            MessageBox.Show(otvet);
+
+    
+        }
+    }
+}
